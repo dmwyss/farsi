1f30c4ed3691ba3921586219d2e95020f6f15399
Moana <moana@Moana.local>
2026-04-30 22:31:27 +1000

    Init first files

---
+++ web/data/vocabPersianRaw.js
@@ -0,0 +1,46 @@
+let vocabPersianRaw = `
+
+kheili *khoshmaze* ast,     it is very *delicious*
+*moshtaagh*e deedanet hastam, *eager* seeyou I-am
+bebakshid *dir* kardam,     sorry I am *late*
+*dorost*e,                  *correct*-that-is
+kheili *mamnoon*,           *thank you* very much
+man kheili *bahoosh*am,     I am very *smart*
+jaaleb,                     interesting
+farsi kheli *aasoon*,       farsi very *easy*-is
+in kif kheili *sangin*e,    this bag is very *heavy*
+emrooz havaa *aali*e,       today weather *great*is
+em*rooz* havaa aalie,       to*day* weather great is
+havaa-to *daaram*,          weather-yours *I-have*
+*havaa* chetore,            *weather* howis
+khoshgel-am,                *darling* (lit beautiful-mine)
+kheyli *khoshgel* hasti,    very *beautiful/handsome* you-are  (lit good shaped)
+toh *fardah*,               until *tomorrow*
+*bah bah* che havaaye khubi, *wonderful* what weather good
+man *gha-veh* dost daram,   I *coffee* like
+*khoshbakht*-am,            lucky/fortunate-I-am
+
+
+bozorg,                     big
+yekam *aroom*tar harf_mizani, a bit *slow*er speaking
+*sir* shodam,               *full* i am
+kheili *mohem* nist,        it is not very *important*
+Sholeh kheili *zerang*e,    Sholeh is very *hardworking*
+*baaroon*ieh,               *rain*ing-is
+toofan,                     storm
+*laazem* nist,              *necessary* is-not
+khande-daar,                funny
+raast-i,                    by the way
+*mo*-haye kheyli khoshgel-i darad, *hair* very beautiful she-has
+khasteh nabaashee,          dont be tired
+
+
+
+khosh-tip,                  stylish/handsome/classy
+khosh amadin,               welcome (lit happy you came)
+khoshgozar,                 having a good time
+khosh-,                     good/pleasant
+kheili *khoshhaal*am dobaare mibinamet, I am *happy* to see you again
+
+
+`
---
+++ web/js/base.js
@@ -0,0 +1,102 @@
+const vocab = {
+    dict: null,
+    list: null, // Sorted array. Gets resorted a lot.
+    sLastSortField: "UNSET",
+    init: function() {
+        this.dict = this.parseRaw();
+        this.initList();
+        this.render();
+    },
+    parseRaw: function() {
+        let oDictOut = {};
+        let asLines = vocabPersianRaw.split("\n");
+        let ixCursor = 0;
+        for (let ix = 0; ix < asLines.length; ix++) {
+            let oWord = this.toWord(asLines[ix]);
+            if (!oWord) {
+                continue;
+            }
+            if (oDictOut.hasOwnProperty(oWord.p.key)) {
+                console.error("trying to set " + oWord.p.key + " again.");
+            }
+            oWord.meta = {
+                ixOrig: ixCursor++,
+                sSortP: oWord.p.key.toLowerCase(),
+                sSortE: oWord.e.key.toLowerCase()
+            };
+            oDictOut[oWord.p.key] = oWord;
+        }
+        return oDictOut;
+    },
+    initList: function() {
+        this.list = [];
+        for (let sKey in this.dict) {
+            this.list.push(this.dict[sKey]);
+        }
+    },
+    toWord: function(sLine) {
+        let asLine = sLine.split(",");
+        if (asLine[0] == "") {
+            return null;
+        }
+        return {
+            p: this.toMot(asLine[0]),
+            e: this.toMot(asLine[1])
+        }
+    },
+    toMot: function(sMotRaw) {
+        sMotRaw = sMotRaw.trim();
+        let as = sMotRaw.split("*");
+        let oOut = {};
+        if (as.length < 3) {
+            oOut.key = as[0];
+            oOut.context = "";
+        } else {
+            oOut.key = as[1];
+            oOut.context = as[0] + "<b class=\"context\">" + as[1] + "</b>" + as[2];
+        }
+        return oOut;
+    },
+    vocabToHtml: function() {
+        let sOut = "<table>"
+        sOut += "<tr><td id=\"sSortP\" class=\"colSorter\" onclick=\"vocab.clickColSort(this);\">"
+            + "persian</td>"
+            + "<td id=\"sSortE\" class=\"colSorter\" onclick=\"vocab.clickColSort(this);\">"
+            + "english</td>"
+            + "<td id=\"ixOrig\" class=\"colSorter\" onclick=\"vocab.clickColSort(this);\">"
+            + "context</td>"
+            + "<td id=\"ixOrig\" class=\"colSorter\" onclick=\"vocab.clickColSort(this);\">"
+            + "context</td>"
+            + "</tr>";
+        for (let ix = 0; ix < this.list.length; ix++) {
+            let oWord = this.list[ix];
+            sOut += "<tr>"
+                + "<td>" + oWord.p.key + "</td>"
+                + "<td>" + oWord.e.key  + "</td>"
+                + "<td>" + oWord.p.context  + "</td>"
+                + "<td>" + oWord.e.context  + "</td>"
+                + "</tr>";
+        }
+        sOut += "</table>";
+        return sOut;
+    },
+    clickColSort: function (uiSrc) {
+//        this.list = this.sort(this.sortCompare)
+        let sSortField = uiSrc.id; // "sSortE";
+        if (sSortField === this.sLastSortField) {
+            this.list.reverse();
+        } else {
+            this.list.sort((a, b) => (a.meta[sSortField] > b.meta[sSortField] ? 1 : -1));
+        }
+        this.sLastSortField = sSortField;
+        this.render();
+    },
+    render: function () {
+        this.uiVocab = document.querySelector("#vocab");
+        this.uiVocab.innerHTML = this.vocabToHtml();
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    vocab.init();
+});
