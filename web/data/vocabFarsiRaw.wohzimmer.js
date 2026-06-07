90e7e5d342c9fc2b9b537930c20308984b6e9b05
dmwyss <dmwyss@gmail.com>
2026-06-07 23:36:05 +1000

    Gradual build.

---
+++ web/data/vocabFarsiRaw.wohzimmer.js
@@ -0,0 +1,148 @@
+let vocabFarsiRaw = `
+
+to kheili zibaa hasti,      you are very beautiful
+*sedaa* aziatet mikone?,     *noise* bothering does?
+een kif kheili *sangin*e,   this bag is very *heavy*
+*daghighan*!,               *correct*!
+een otaagh kheli *boland*,   this room is too *noisy*
+kheili *khoshmaze* ast,     it is very *delicious*
+*moshtaagh*e deedanet hastam, *eager* seeyou I-am
+bebakshid *dir* kardam,     sorry I am *late*
+*dorost*e,                  *correct*-that-is
+kheili *mamnoon*,           *thank you* very much
+man kheili *baahoosh*am,     I am very *smart*
+jaaleb,                     interesting
+farsi kheli *aasoon*,       farsi very *easy*-is
+in kif kheili *sangin*e,    this bag is very *heavy*
+emrooz havaa *aali*e,       today weather *great*is
+em*rooz* havaa aalie,       to*day* weather great is
+havaa-to *daaram*,          weather-yours *I-have*
+*havaa* chetore,            *weather* howis
+khoshgel-am,                *darling* (lit beautiful-mine)
+kheyli *khoshgel* hasti,    very *beautiful/handsome* you-are  (lit good shaped)
+toh *fardah*,               until *tomorrow*
+*bah bah* che havaaye khubi, *wonderful* what weather good
+man *gha-veh* dost daram,   I *coffee* like
+*khoshbakht*-am,            lucky/fortunate-I-am
+Sholeh *doost daram*,       Sholeh *I like*
+*zahmat* nakeshid,          *trouble* dont-pull
+
+# Ling
+*hal*-et chetore,           *mood/feeling*-you howis?
+mishe *tekraar konid*?,     can-possible *repeat*?
+man nemifahmmam (*fahmidan*),  i not-understand-me *to understand*
+mishe dobare *aarum* begid?,   can-possible again *slow* say?
+hatman,                     sure
+
+
+*movafagh* beshi,           *success*ful be-you
+bozorg,                     big
+yekam *aroom*tar harf_mizani, a bit *slow*er speaking
+*sir* shodam,               *full* i am
+kheili *mohem* nist,        it is not very *important*
+Sholeh kheili *zerang*e,    Sholeh is very *hardworking*
+*baaroon*ieh,               *rain*ing-is
+toofan,                     storm
+*laazem* nist,              *necessary* is-not
+khande-daar,                funny
+raast-i,                    by the way
+*mo*-haye kheyli khoshgel-i darad, *hair* very beautiful she-has
+khasteh nabaashee,          dont be tired
+*hichi*,                    nothing
+chizi,                      something
+baashe,                     ok/sure
+*zood* miresam,             *close* i am (find word for word xlation)
+*moshtaagh*-e didaar,	    i am *eager* to see you.
+*haste-i*?,                 *ok-you*?
+*eynak*am ku? Ru saret-e,   *glasses*-my where? On head-is
+ghoorbaagheh,               frog
+*zood* mi-ris-am,            *fast/soon/early/quick* definitely-arriving-am
+hoselam sar rafte,          I am bored
+daaram *mimiram*,           having *dying*-me
+
+khosh-tip,                  stylish/handsome/classy
+khosh amadin,               welcome (lit happy you came)
+khoshgozar,                 having a good time
+khosh-,                     good/pleasant
+kheili *khoshhaal*am dobaare mibinamet, I am *happy* to see you again
+
+budan,                      verb to be
+hastam,                     I am
+hasti,                      you are (sing.)
+hast,                       he/she/it is
+hastim,                     we are
+hastid,                     you are (pl.)
+hastand,                    they are
+# Note: In colloquial speech, the 3rd person singular "ast" becomes "-e" and the 3rd person plural "and" often becomes "-an".
+
+man budam,                  i was
+to budi,                    you were (sing.)
+u bud,                      he/she/it was
+maa budim,                  we were: budim
+shomaa budid,               you were (pl./formal)
+aanhaa budand,              they were
+
+# Present stem kon, past stem kard
+kardan,                     verb to do
+man mikonam,                i do
+to mikoni,                  you(singl) do
+u mikonad/mikoneh,          he/she/it does
+maa mikonim,                we do
+shomaa mikonid/mikonin,     you(plur) do
+aanhaa/ishaan mikonand/mikonan,  they do
+
+man *na-kardam*,              i *did-not-do* (past)
+man *ne-mikonam*,             i *do-not-do* (imperf)
+
+# Compound verbs based on Kardan:
+zendegi kardan,		        to live
+fekr kardan,			    to think
+kār kardan,			        to work
+sohbat kardan,		        to speak
+
+
+bah bah,			    	oh wow beautiful
+nush-e jaan,			    en guete
+azizam,				        my darling.
+mobaarake,			        congratulations
+khaste nabaashin,	        means dont be tired
+*moshtaagh*-e didaar,	    i am *eager* to see you.
+felan, 				        *for now* meaning see you soon
+zibaa,                      beautiful
+
+
+
+shaad,                      happy
+diidan,                     to see
+raa,                        object marker
+behh,                       to
+baa,                        with
+chaandan,                   to read
+neveshtan,                  to write
+az,                         from
+vah,                        and
+een,                        that
+mohandes,                   engineer
+peseshk,                    doctor
+mesaad,                     example
+dafeye dige to *hesaab kon*,  next time you *pay-do*
+
+
+dafeye ba’ad,               next time
+dafeye dige,                another time or next time
+dafeye aval,                the first time
+dafeye akhar,               the last time
+
+negah kon,                  look do
+goush kon,                  listen / ear do
+sokout kon,                 be quiet / silence do
+booye khoob mideh,          smell good gives
+sabr kon,                   patience do
+midunam chi migi,           know what you say/understand
+mee-fah-mam,                understanding-i-am
+fah-mee-dam,                understood-i-did
+
+
+
+
+`
