59267c0b1a8551dabf738b58cca0bf5ac8f61633
dmwyss <dmwyss@gmail.com>
2026-07-17 19:52:40 +1000

    Gradual build.

---
+++ web/data/vocabFarsiRaw.wohzimmer.js
@@ -0,0 +1,32 @@
+
+
+harchi zoodtar behtar,  everywhat sooner better
+che behtar,             even better
+dahanam aab oftaad,     my mouth is watering
+
+------------
+corrected by sholeh
+------
+Ye daastaan daaram ke mikham begam.
+I have a story I would like to tell.
+
+Kalameye "tulip" az faarsiye baastaan miayad, vali **na** az kalameye "laleh".
+The word tulip comes from ancient Farsi. But **not** from the word "laleh".
+
+Tooye Iraane baastaan, oonaa fekr mikardan ke in gol shabihe ye ammameye koocholooe.
+In ancient Persia they thought the flower looked like a little turban.
+
+Oonaa az kalameye ghadimie faarsiye "dulband" estefaade mikardan.
+They used the old Persian word "dulband".
+
+Tooye Torkiye in kalame shod "tülbent".
+In Turkie the word became "tülbent".
+
+Tooye Aalmaan in kalame shod "tülpe".
+In Germany the word became "tülpe".
+
+Pas be engelisi mishe "tulip".
+Then "tulip" in English.
+
+Pas esme khiaaboonetoof  naz ye kalameye faarsi miaad.
+So your street name comes from a Persian word.
