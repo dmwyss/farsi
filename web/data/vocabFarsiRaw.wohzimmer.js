c943b0dce68bb94b21f67cbac3608541063e87f7
dmwyss <dmwyss@gmail.com>
2026-08-01 00:24:49 +1000

    Gradual build.

--- web/data/vocabFarsiRaw.wohzimmer.js
+++ web/data/vocabFarsiRaw.wohzimmer.js
@@ -9,6 +9,5 @@ translate the following sentence into informal farsi:
 
 The old Persian word created the Turkish word: "tülbent".
 
-
 Oon kalameye ghadimie farsie, kalameye „tülbent“-e torkiro dorost kard.
 Oon kalameye ghadimie farsie, in kalameye torkiro dorost kard: tülbent.
