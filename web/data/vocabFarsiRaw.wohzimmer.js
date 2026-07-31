3f1fa1e3e02064719d23a2d6608d38a99f9f3d85
dmwyss <dmwyss@gmail.com>
2026-08-01 00:23:48 +1000

    just the wohnzimmer

--- web/data/vocabFarsiRaw.wohzimmer.js
+++ web/data/vocabFarsiRaw.wohzimmer.js
@@ -1,4 +1,14 @@
 
-qerqere,                gargle
+qerqere,                    gargle
+
+change this one for         you saw (past)
+diddi - cheghadr aasoone,   see - how easy that is
 
 
+translate the following sentence into informal farsi:
+
+The old Persian word created the Turkish word: "tülbent".
+
+
+Oon kalameye ghadimie farsie, kalameye „tülbent“-e torkiro dorost kard.
+Oon kalameye ghadimie farsie, in kalameye torkiro dorost kard: tülbent.
