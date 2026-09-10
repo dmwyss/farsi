832a6ac46a2c85156a75c045010a0e140dce9d3e
David Wyss <dmwyss@gmail.com>
2026-09-10 15:53:11 +1000

    Continual updates.

---
+++ web/audioPlayer/trackListData.js
@@ -0,0 +1,6 @@
+let trackListData = [
+    {file: "Vocab_01_-_Laazem_20.mp3",              secs: 150},
+    {file: "Vocab_04_-_Laazem_60.mp3",              secs: 513},
+    {file: "Vocab_05_-_Laazem_Essential_100.m4a",   secs: 821},
+    {file: "Vocab_06_-_Vaajeb_Mandatory_60.mp3",    secs: 804}
+];
